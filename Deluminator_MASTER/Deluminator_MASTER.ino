ef3442b77cad1007605ef0d6579bab8db4aca23b
Tyler Hobbs <tyler.hobbs@duke.edu>
2013-09-05 17:52:32 -0400

    not complete changes

--- Deluminator_MASTER/Deluminator_MASTER.ino
+++ Deluminator_MASTER/Deluminator_MASTER.ino
@@ -2,13 +2,23 @@
  * Created by Tyler Hobbs, Lucy Corippo, Kavitha Prasanna, Lauren Oliveri
  * September 5, 2013
  */
+#include <ColorLCDShield.h>
+int lcd_key;
 
-#include ColorLCDShield
+//Defines buttons for user interface
+#define btnNONE 0
+#define btnS1 1
+#define btnS2 2
+#define btnS3 3
+
+LCDShield lcd;
 
 void setup() {
   Serial.begin(9600);
-  LCDShield lcd;
-
+  /* Initialize the LCD, set the contrast, clear the screen */
+  lcd.init(PHILIPS);
+  lcd.contrast(-51);
+  lcd.clear(BLACK);
 }
 
 void loop() {
@@ -34,4 +44,5 @@ void loop() {
     }
   }
 }
+
 
