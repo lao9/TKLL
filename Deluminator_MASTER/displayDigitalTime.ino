87aaf0c8470e3889aca6eac5106929ecc4e6a07d
Tyler Hobbs <tyler.hobbs@duke.edu>
2013-09-08 14:13:22 -0400

    Adding displayDigitaltime functionality.

--- Deluminator_MASTER/displayDigitalTime.ino
+++ Deluminator_MASTER/displayDigitalTime.ino
@@ -1,14 +1,21 @@
 void displayDigitalTime(int s, int ts)
 {
   char timeChar[4] = {
-  'x', 'x', -2, 'x'};  
-  
+    'x', 'x', -2, 'x'  };  
+
   // Turn the values into idividual integers
   timeChar[0] = s/10;
   timeChar[1] = s%10;
   timeChar[3] = ts;
 
+  for (int i=0; i<4; i++) {
+    timeChar[i] += 48;
+    Serial.print(timeChar[i]);
+  }
 
+  /* Print the time on the clock */
+  lcd.setStr(timeChar, 55, 30, CORAL, BLACK);
   
-  
+
 }
+
