eac95f561ea874e707a92a9da358cd9d4a73c6c8
Tyler Hobbs <tyler.hobbs@duke.edu>
2013-09-08 16:07:53 -0400

    What the version 2. Yes, its the version 2.

---
+++ Deluminator_MASTER/Deluminator_MASTERv2/Deluminator_MASTERv2.ino
@@ -0,0 +1,124 @@
+/** Deluminator Device (LED Brighter/Dimmer) Code
+ * Created by Tyler Hobbs, Lucy Corippo, Kavitha Prasanna, Lauren Oliveri
+ * September 5, 2013
+ */
+
+#include <ColorLCDShield.h>
+int lcd_key;
+int seconds, tenthsecs;
+boolean firstTime = true;
+int startTime, currTime;
+
+#define SECONDS 00
+#define TENTHSECS 0
+
+#define btnNONE 2
+#define btnS1 3
+#define btnS2 4
+#define btnS3 5
+
+LCDShield lcd;
+
+void setup() {
+  Serial.begin(9600);
+  /* Initialize the LCD, set the contrast, clear the screen */
+  lcd.init(PHILIPS);
+  lcd.contrast(-51);
+  lcd.clear(BLACK);
+
+  // Set the input Pins
+  pinMode(btnS1,INPUT);
+  pinMode(btnS2,INPUT);
+  pinMode(btnS3,INPUT);
+
+  // Set the time variables
+  seconds = SECONDS;
+  tenthsecs = TENTHSECS;
+}
+
+void loop() {
+
+  lcd_key = buttonPress();
+
+  switch (lcd_key)
+  {
+  case btnNONE:
+    {
+      displayDigitalTime(seconds,tenthsecs);
+      break;
+    }
+  case btnS1: 
+    { //TIME INCREASE
+      if (firstTime) {
+        startTime = millis();
+        firstTime = !firstTime;
+      }
+
+      currTime = millis() - startTime;
+
+      if (currTime > 76) {
+        if (tenthsecs < 9) {
+          tenthsecs += 1;
+        }
+        else {
+          seconds += 1;
+          tenthsecs = 0;
+        }
+        firstTime = !firstTime; 
+      }
+      displayDigitalTime(seconds,tenthsecs);
+      if (seconds >= 30) {
+        displayDigitalTime(30,0);
+        delay(500);
+        lcd.clear(WHITE);
+        displayFun();
+      }
+      break;
+    }
+  case btnS2:
+    { //TIME DECREASE
+      if (firstTime) {
+        startTime = millis();
+        firstTime = !firstTime;
+      }
+
+      currTime = millis() - startTime;
+
+      if (currTime > 76) {
+        if (seconds > 0) {
+          if (tenthsecs > 0) {
+            tenthsecs -= 1;
+          }
+          else if (tenthsecs == 0) {
+            seconds -= 1;
+            tenthsecs = 9;
+          } 
+        }
+        else {
+          if (tenthsecs > 0) {
+            tenthsecs -= 1;
+          }
+          else if (tenthsecs == 0) {
+            tenthsecs = 0;
+          } 
+        }
+        firstTime = !firstTime; 
+      }
+      break;
+    }
+  case btnS3:
+    {
+      break;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
