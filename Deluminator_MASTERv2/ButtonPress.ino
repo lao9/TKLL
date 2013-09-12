6e1492d99d354ba5648556fac1f829e8a1cc85f4
Tyler Hobbs <tyler.hobbs@duke.edu>
2013-09-11 23:09:39 -0400

    Commented code in buttonPress and in displayDigitalTime

--- Deluminator_MASTERv2/ButtonPress.ino
+++ Deluminator_MASTERv2/ButtonPress.ino
@@ -1,36 +1,40 @@
 /* Button Press
  This code is used to obtain user input from the buttons on the Arduino Color LCD shield
  Each button pin is set HIGH and then the buttons are read and trigger input when in a LOW state (pressed)
- There is a delay associated with each button read to eliminate bouncing
  
  Takes no input variables; executes based on reading button pin states
  */
 
 int buttonPress()
 {  
+  // Write all of the buttons HIGH first to check if they have been turned low == "pressed"
   digitalWrite(btnS1,HIGH);
   digitalWrite(btnS2,HIGH);
   digitalWrite(btnS3,HIGH);
 
-  if(!digitalRead(btnS1)) // If the 1st button pin reads LOW, a press of the 1st button is returned
+  if(!digitalRead(btnS1)) 
+    // If the S1 button pin reads LOW, a press of the S1 button is returned
   {
-   // delay(50);
     return(btnS1);
   }
-  else if(!digitalRead(btnS2)) // If the 2nd button pin reads LOW, a press of the 2nd button is returned
+  else if(!digitalRead(btnS2)) 
+    // If the S2 button pin reads LOW, a press of the S2 button is returned
   {
     return(btnS2);
   }
-  else if(!digitalRead(btnS3)) // If the 3rd button pin reads LOW, a press of the 3rd button is returned
+  else if(!digitalRead(btnS3)) 
+    // If the S3 button pin reads LOW, a press of the S3 button is returned
   {
     return(btnS3);
   }
   else if(!digitalRead(btnS1) && !digitalRead(btnS2))
+    // If the S1 & S2 button pins read LOW, no button press is returned
   {
     return(btnNONE);
   }
-  else
+  else  // default: return NO button press
   {
     return(btnNONE);
   }
 }
+
--- Deluminator_MASTERv2/displayDigitalTime.ino
+++ Deluminator_MASTERv2/displayDigitalTime.ino
@@ -1,12 +1,18 @@
+/* displayDigitalTime function takes time variable inputs and sorts them into a character string
+ to print to the LCD screen.
+ displayDigitalStats function takes the total tenth-seconds variable and displays statistics to
+ the LCD screen
+ */
+
 void displayDigitalTime(int s, int ts)
 {
-  char timeChar[8] = {
-    s/10, s%10, -2, ts};
+  char timeChar[8] = {  // put the time variables in a character string (XX.X)
+    s/10, s%10, -2, ts  };
 
-  for (int i=0; i<4; i++) {
+  for (int i=0; i<4; i++) {  // Convert the character string into ASCII numbers
     timeChar[i] += 48;
   }
-  
+
   /* Print the time on the clock */
   lcd.setStr(timeChar, 75, 50, WHITE, BLACK);
 }
@@ -14,11 +20,12 @@ void displayDigitalTime(int s, int ts)
 void displayDigitalStats(int ts)
 {
   char timeChar[8] = {
-    -2, ts};
-    
-    for (int i=0; i<2; i++){
-      timeChar[i] += 48;
-    }
-    
-    lcd.setStr(timeChar, 75, 50, WHITE, BLACK);
+    -2, ts  };
+
+  for (int i=0; i<2; i++){
+    timeChar[i] += 48;
+  }
+
+  lcd.setStr(timeChar, 75, 50, WHITE, BLACK);
 }
+
