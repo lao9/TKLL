665809c156cc84b524d682d9b13d8ccf6eab9cc5
Tyler Hobbs <tyler.hobbs@duke.edu>
2013-09-12 00:00:01 -0400

    Tyler changes made to comments

--- Deluminator_MASTERv2/statistics.ino
+++ Deluminator_MASTERv2/statistics.ino
@@ -4,24 +4,25 @@ void statistics()
     previous = millis();
     first = !first;
   }
-//the stat_time is assumed to updated every .1 second
-//therefore, if we compare the previous stat_time to the current time
-//we can take the average and standard deviation it should be around .1
+  //the stat_time is assumed to updated every .1 second
+  //therefore, if we compare the previous stat_time to the current time
+  //we can take the average and standard deviation it should be around .1
   counter = counter + 1;
   tenthsec_int = stat_time - previous;
   previous = stat_time;
-  tot_tenthsec = tot_tenthsec + tenthsec_int;
-  
+  tot_tenthsec += tenthsec_int;
+
   //error
   unsigned long error = tenthsec_int - .1;
   unsigned long square_error = pow(error, 2);
-  sq_error_sum = sq_error_sum + square_error;
+  sq_error_sum += square_error;
 }
 
 void calc()
 {
-  
+
   avg_tenthsec = tot_tenthsec/counter;
   unsigned long avg_sq_error = sq_error_sum/counter;
   st_dev = sqrt(avg_sq_error);
 }
+
